243c63de2a44d1b5b719be7a42a10281762673b5
andriyan <rsnkapandri@gmail.com>
2017-01-24 07:02:02 -0800

    perubahan pertama

---
+++ HitungLuas.java
@@ -0,0 +1,15 @@
+public class HitungLuas
+{
+	public static void main(String[] args) 
+	{
+		/*int adalah tipe */
+	int panjang = 5;
+	int lebar = 2;
+	float luas;
+	System.out.println("pangjangnya"+panjang);
+	System.out.println("lebarnya"+lebar);
+luas = (float)(panjang*lebar);
+	System.out.println("luasnya yaitu"+panjang*lebar);
+
+	}
+}
